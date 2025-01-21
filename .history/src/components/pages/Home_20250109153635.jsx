06192d6218ba3f5808a8c172514982a916e177e2
reynard <reynardnathaniel@gmail.com>
2025-01-22 00:31:05 +0900

    Updates

---
+++ .history/src/components/pages/Home_20250109153635.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../Card.jsx/Card";
+import Card2 from "../Card2/Card2";
+import Nav from "../Navigation/Nav";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <div className="test">
+        <Card />
+        {/* <hr className="line" /> */}
+        <Card2 />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
